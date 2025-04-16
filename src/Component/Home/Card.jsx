4f9cbc3c000f98fbac7e7d4d745f8eb8fc1fec05
abusayedsohagislam <abusayedsohagislam25@gmail.com>
2025-04-17 02:50:30 +0600

    campaign card modify and get responsive

--- src/Component/Home/Card.jsx
+++ src/Component/Home/Card.jsx
@@ -7,13 +7,21 @@ const Card = ({ card }) => {
 
 
     return (
-        <div>
-            <div className="card card-compact bg-base-100 shadow-xl">
-                {/* <figure><img src="" alt="Shoes" /></figure> */}
-                <div className="card-body">
-                    <h2 className="card-title">{card._id}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
+        <div className='h-min-[450px]'>
+            <div className="card card-compact h-full bg-base-100 shadow-xl">
+                <figure>
+                    <img 
+                    src={card.image} 
+                    alt="Shoes"
+                    className='h-48 w-full'
+                    />
+                </figure>
+                <div className="card-body justify-between">
+                    <h2 className="card-title">{card.title}</h2>
+                    <h1>{card.description}</h1>
+                    <h1><span className='font-semibold'>Deadline:</span> {card.deadline}</h1>
+                    <h2><span className='font-semibold'>Min. Donation:</span> {card.amount} BDT</h2>
+                    <div className="card-actions">
                         <Link to={`/details/${card._id}`}><button className="btn btn-primary">See More</button></Link>
                     </div>
                 </div>
--- src/Component/Home/Cards.jsx
+++ src/Component/Home/Cards.jsx
@@ -4,14 +4,13 @@ import Card from './Card';
 
 const Cards = () => {
 
-
     const cards = useLoaderData();
 
     return (
-        <div className='w-11/12 mx-auto my-5'>
-            <h1 className='text-center'>Our Present Campaings</h1>
+        <div className='w-10/12 md:w-11/12 mx-auto my-5'>
+            <h1 className='text-center  text-2xl font-bold'>Our Present Campaings</h1>
 
-            <div className='grid grid-cols-3 mt-6 gap-3'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 mt-6 gap-3'>
                 {
                     cards.map(card => <Card card = {card} key = {card._id}></Card>)
                 }
