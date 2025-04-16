f6e4ed4358b4aece3a63ebd888b99767308d5b5a
abusayedsohagislam <abusayedsohagislam25@gmail.com>
2025-04-16 17:21:23 +0600

    banner modify and responsive

--- src/Component/Banner.jsx/Banner.jsx
+++ src/Component/Banner.jsx/Banner.jsx
@@ -30,12 +30,22 @@ const Banner = () => {
 
     return (
         <div className='my-4'>
-            <Carousel responsive={responsive}>
-                <div className='flex mx-auto'><img  className='flex mx-auto' src="https://i.pinimg.com/736x/a9/a3/49/a9a349853cc1bf106c56068522641133.jpg" alt="" /></div>
-                <div className='flex mx-auto'><img  className='flex mx-auto' src="https://i.pinimg.com/736x/a9/a3/49/a9a349853cc1bf106c56068522641133.jpg" alt="" /></div>
-                <div className='flex mx-auto'><img  className='flex mx-auto' src="https://i.pinimg.com/736x/a9/a3/49/a9a349853cc1bf106c56068522641133.jpg" alt="" /></div>
-                <div className='flex mx-auto'><img  className='flex mx-auto' src="https://i.pinimg.com/736x/a9/a3/49/a9a349853cc1bf106c56068522641133.jpg" alt="" /></div>
-                <div className='flex mx-auto'><img  className='flex mx-auto' src="https://i.pinimg.com/736x/a9/a3/49/a9a349853cc1bf106c56068522641133.jpg" alt="" /></div>
+            <Carousel
+
+                swipeable={false}
+                draggable={false}
+                showDots={true}
+                responsive={responsive}
+                ssr={true}
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={3000}
+                keyBoardControl={true}
+            >
+                <div className='flex mx-auto'><img className='flex mx-auto border-4 border-blue-500 rounded-xl w-10/12 md:w-auto' src="https://i.ibb.co.com/BV9DTwhj/Slide1.jpg" alt="" /></div>
+                <div className='flex mx-auto'><img className='flex mx-auto border-4 border-blue-500 rounded-xl w-10/12 md:w-auto' src="https://i.ibb.co.com/svvgGkbh/Slide2.jpg" alt="" /></div>
+                <div className='flex mx-auto'><img className='flex mx-auto border-4 border-blue-500 rounded-xl w-10/12 md:w-auto' src="https://i.ibb.co.com/b5FPFjNB/Slide3.jpg" alt="" /></div>
+                <div className='flex mx-auto'><img className='flex mx-auto border-4 border-blue-500 rounded-xl w-10/12 md:w-auto' src="https://i.ibb.co.com/HLV9vpkn/Slide4.jpg" alt="" /></div>
             </Carousel>;
         </div>
     );
