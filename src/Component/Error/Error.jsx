31b1b436ec91409fffdfe3ba48bff5d5dd5c71c3
abusayedsohagislam <abusayedsohagislam25@gmail.com>
2025-04-19 20:31:45 +0600

    Error Page Update

--- src/Component/Error/Error.jsx
+++ src/Component/Error/Error.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
         <div className='h-screen relative'>
-            <div className='absolute top-0 left-0 w-full h-1/2 bg-slate-700 z-10 rounded-b-full shadow-lg'></div>
-            <div className='absolute bottom-0 left-0 w-full h-1/2 bg-blue-600 z-10 rounded-t-full shadow-lg'></div>
-
-            <div className='relative z-20 flex justify-center items-center flex-col h-full gap-10 md:gap-5'>
-                <h1 className='text-4xl md:text-8xl text-white font-black'>ERROR</h1>
-                <h1 className='text-4xl md:text-8xl text-white font-black'>404</h1>
+            <div className='absolute top-0 left-0 w-full h-1/2 bg-slate-700 z-10 rounded-b-full shadow-lg flex justify-center items-end'>
+                <h1 className='text-4xl md:text-8xl text-white font-black pb-3'>ERROR</h1>
             </div>
-
-            <button className='relative z-3'>BBBB</button>
+            <div className='absolute bottom-0 left-0 w-full h-1/2 bg-blue-600 z-30 rounded-t-full shadow-lg flex justify-start flex-col items-center'>
+                <h1 className='text-4xl md:text-8xl text-white font-black'>404</h1>
+                <br />
+                <Link to="/" className='btn btn-secondary'>Go to Home</Link>
+            </div>
         </div >
     );
 };
