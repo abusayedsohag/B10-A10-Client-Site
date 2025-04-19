5e9c57bcef5aae1b0669ed90930ea5806521ed71
abusayedsohagislam <abusayedsohagislam25@gmail.com>
2025-04-19 17:17:23 +0600

    error page update

--- src/Component/Error/Error.jsx
+++ src/Component/Error/Error.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div className='flex justify-center items-center h-screen flex-col'>
-            <h1 className='text-9xl font-black'>ERROR</h1>
-            <h1 className='text-9xl font-black'>404</h1>
-        </div>
+        <div className='h-screen relative'>
+            <div className='absolute top-0 left-0 w-full h-1/2 bg-slate-700 z-10 rounded-b-full shadow-lg'></div>
+            <div className='absolute bottom-0 left-0 w-full h-1/2 bg-blue-600 z-10 rounded-t-full shadow-lg'></div>
+
+            <div className='relative z-20 flex justify-center items-center flex-col h-full gap-10 md:gap-5'>
+                <h1 className='text-4xl md:text-8xl text-white font-black'>ERROR</h1>
+                <h1 className='text-4xl md:text-8xl text-white font-black'>404</h1>
+            </div>
+
+            <button className='relative z-3'>BBBB</button>
+        </div >
     );
 };
 
