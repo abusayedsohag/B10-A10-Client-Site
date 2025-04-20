7fd19841f3c5b925650e503b88a900a302b7c0ac
abusayedsohagislam <abusayedsohagislam25@gmail.com>
2025-04-20 22:15:56 +0600

    add a typewriter effect and modify some

--- src/Component/Home/Cards.jsx
+++ src/Component/Home/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Card from './Card';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Cards = () => {
 
@@ -8,7 +9,20 @@ const Cards = () => {
 
     return (
         <div className='w-10/12 md:w-11/12 mx-auto my-5'>
-            <h1 className='text-center text-2xl font-bold'>Our Present Campaings</h1>
+            <h1 className='text-center dark:text-white text-xl md:text-3xl font-bold'>
+                Our Present{' '}
+                <span className='text-bold text-red-600'>
+                    <Typewriter
+                        words={['Initiative', 'Drive', 'Effort', 'Project', 'Mission', 'Program', 'Operation', 'Movement']}
+                        loop={100}
+                        cursor
+                        cursorStyle='__'
+                        typeSpeed={100}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    />
+                </span>
+            </h1>
 
             <div className='grid md:grid-cols-2 lg:grid-cols-3 mt-6 gap-3'>
                 {
